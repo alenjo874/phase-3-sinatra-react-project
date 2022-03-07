04c781fcd788be3987bc2a036602331a4ce310b9
Alen Jo <alenjo874@gmail.com>
2022-03-07 13:58:49 -0500

    crud for studysessions and notes controller

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -4,5 +4,21 @@ class NotesController < Sinatra::Base
       notes = Note.all
       notes.to_json
     end
+
+    post "/notes" do
+      new_note = Note.create(params)
+      new_note.to_json
+    end
+
+    patch "/notes/:id" do
+      edit_note = Note.find(params[:id])
+      edit_note.update(params)
+      edit_note.to_json
+    end
+
+    delete "/notes/:id" do
+      delete_note = Note.find(params[:id])
+      delete_note.destroy
+    end
   
   end
--- app/controllers/study_sessions_controller.rb
+++ app/controllers/study_sessions_controller.rb
@@ -4,5 +4,21 @@ class StudySessionsController < Sinatra::Base
         study_sessions = StudySession.all
         study_sessions.to_json
     end
+
+    post "/study_sessions" do
+        new_study_session = StudySession.create(params)
+        new_study_session.to_json
+      end
+  
+      patch "/study_sessions/:id" do
+        edit_study_session = StudySession.find(params[:id])
+        edit_study_session.update(params)
+        edit_study_session.to_json
+      end
+  
+      delete "/study_sessions/:id" do
+        delete_study_session = StudySession.find(params[:id])
+        delete_study_session.destroy
+      end
   
   end
